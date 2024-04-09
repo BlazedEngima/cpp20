1df8fd6989a7143da5238fcbcc75ed060944e34b
Blazed <samueltheofie@gmail.com>
2024-04-09 12:38:10 +0700

    Added basic constructors
    Copy constructor needs to be changed so list uses iterators from copied dict

--- cache/include/lrucache.hpp
+++ cache/include/lrucache.hpp
@@ -1,15 +1,18 @@
 #ifndef LRU_CACHE_H
 #define LRU_CACHE_H
 
+#include <cstdint>
 #include <functional>
 #include <list>
 #include <unordered_map>
+
+#define DEFAULT_SIZE 5000
 
 namespace cache {
 
 template <typename Key>
 concept Hashable = requires(Key key) {
-    { std::hash<Key>{}(key) } -> std::convertible_to<std::size_t>;
+    { std::hash<Key> {}(key) } -> std::convertible_to<std::size_t>;
 };
 
 template <typename Key, typename Value>
@@ -22,11 +25,37 @@ using List = std::list<T>;
 template <typename Key, typename Val>
 class LRUCache {
 public:
+    LRUCache() noexcept;
+    LRUCache(std::uint64_t) noexcept;
+    LRUCache(const LRUCache &) noexcept;
+    LRUCache(LRUCache &&) noexcept;
+
 private:
+    std::uint64_t size;
     Dict<Key, Val> map;
     List<decltype(map.begin())> order;
 };
 
-}  // namespace cache
+template <typename Key, typename Val>
+LRUCache<Key, Val>::LRUCache() noexcept : size(DEFAULT_SIZE) {}
 
-#endif  // LRU_CACHE_H
+template <typename Key, typename Val>
+LRUCache<Key, Val>::LRUCache(std::uint64_t len) noexcept : size(len) {}
+
+template <typename Key, typename Val>
+LRUCache<Key, Val>::LRUCache(const LRUCache<Key, Val> &cache) noexcept {
+    this->size = cache.size;
+    this->map = cache.map;
+    this->order = cache.order;
+}
+
+template <typename Key, typename Val>
+LRUCache<Key, Val>::LRUCache(LRUCache<Key, Val> &&cache) noexcept {
+    this->size = cache.size;
+    this->map = cache.map;
+    this->order = cache.order;
+}
+
+} // namespace cache
+
+#endif // LRU_CACHE_H
