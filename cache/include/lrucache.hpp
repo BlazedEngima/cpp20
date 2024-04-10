09ed0fdb74b19d0596b67874e62ae8e32b77dd09
Blazed <samueltheofie@gmail.com>
2024-04-10 12:27:08 +0700

    Fixed copy constructor and added Pair template

--- cache/include/lrucache.hpp
+++ cache/include/lrucache.hpp
@@ -23,6 +23,10 @@ template <typename T>
 using List = std::list<T>;
 
 template <typename Key, typename Val>
+    requires Hashable<Key>
+using Pair = std::pair<Key, Val>;
+
+template <typename Key, typename Val>
 class LRUCache {
 public:
     LRUCache() noexcept;
@@ -30,25 +34,34 @@ public:
     LRUCache(const LRUCache &) noexcept;
     LRUCache(LRUCache &&) noexcept;
 
+    auto emplace(Key, Val) -> void;
+    auto insert(Pair<Key, Val>) -> void;
+
 private:
     std::uint64_t size;
     Dict<Key, Val> map;
     List<decltype(map.begin())> order;
 };
 
+/* Constructor */
 template <typename Key, typename Val>
 LRUCache<Key, Val>::LRUCache() noexcept : size(DEFAULT_SIZE) {}
 
 template <typename Key, typename Val>
 LRUCache<Key, Val>::LRUCache(std::uint64_t len) noexcept : size(len) {}
 
+/* Copy Constructor */
 template <typename Key, typename Val>
 LRUCache<Key, Val>::LRUCache(const LRUCache<Key, Val> &cache) noexcept {
     this->size = cache.size;
     this->map = cache.map;
-    this->order = cache.order;
+    for (const auto &it : cache.order) {
+        Key key = it->first;
+        this->order.insert(this->map.find(key));
+    }
 }
 
+/* Move Constructor */
 template <typename Key, typename Val>
 LRUCache<Key, Val>::LRUCache(LRUCache<Key, Val> &&cache) noexcept {
     this->size = cache.size;
