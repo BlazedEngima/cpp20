c0576892923fb89690b1ab88607ba2b85277be96
Blazed <samueltheofie@gmail.com>
2024-04-10 14:48:28 +0700

    Added comments

--- cache/test/test.cpp
+++ cache/test/test.cpp
@@ -3,12 +3,15 @@
 #include <gtest/gtest.h>
 #include <string>
 
+/* --------------- Test Setup --------------- */
+/* Type struct encapsulation */
 template <typename Key, typename Val>
 struct TypeDefinitions {
     typedef Key key;
     typedef Val val;
 };
 
+/* Class fixture test */
 template <typename types>
 class LRUCacheTest : public ::testing::Test {
 protected:
@@ -18,12 +21,15 @@ protected:
     cache::LRUCache<typename types::key, typename types::val> _cache;
 };
 
+/* Defining types to test */
 using testing::Types;
 using String = std::string;
 typedef Types<TypeDefinitions<int, String>, TypeDefinitions<String, String>,
               TypeDefinitions<char, char>>
     HashTypes;
+/* --------------- Test Setup Ends Here --------------- */
 
+/* Tests start here */
 TYPED_TEST_SUITE(LRUCacheTest, HashTypes);
 
 TYPED_TEST(LRUCacheTest, BasicConstructor) {
